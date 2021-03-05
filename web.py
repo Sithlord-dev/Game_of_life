8d0c4665f263b883a89e1b279b7070b98b249dfa
Mweya Ruider <mweya@orcachat.com>
2021-03-05 17:07:33 +0100

    Very rough web interface

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 import os
 from main import GIFGenerator
 from pydantic import BaseSettings
@@ -23,6 +24,11 @@ async def generate_gif(probability:float=0.5, h_size:int=10, w_size:int=10):
     g = GIFGenerator()
     filename = g.generateGIF(probability, (h_size,w_size))
     return {"filename": filename+".gif", "url": f"http://{settings.SERVER_HOST}:{settings.SERVER_PORT}/temp/{filename}.gif"}
+
+@app.get("/index.html", response_class=HTMLResponse)
+async def get_homepage():
+    html_content = open("index.html", "r").read()
+    return HTMLResponse(content=html_content, status_code=200)
 
 # Return app info if the root is called
 @app.get("/")
